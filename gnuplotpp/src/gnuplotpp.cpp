eb3e9669b2e409419b6507501fcb2cf757e4d4f4
Luca Ciucci <LucaCiucci@users.noreply.github.com>
2021-10-07 18:39:12 +0200

    notice

--- gnuplotpp/src/gnuplotpp.cpp
+++ gnuplotpp/src/gnuplotpp.cpp
@@ -1,4 +1,13 @@
 /* LC_NOTICE_BEGIN
+===============================================================================
+|                        Copyright (C) 2021 Luca Ciucci                       |
+|-----------------------------------------------------------------------------|
+| Important notices:                                                          |
+|  - This work is distributed under the MIT license, feel free to use this    |
+|   work as you wish.                                                         |
+|  - Read the license file for further info.                                  |
+| Written by Luca Ciucci <luca.ciucci99@gmail.com>, 2021                      |
+===============================================================================
 LC_NOTICE_END */
 
 #include <gnuplotpp/gnuplotpp.hpp>
